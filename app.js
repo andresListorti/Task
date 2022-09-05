8abcafcca32fdfbc7142352843bd91de3ba16a1b
andresListorti <andreslistorti@gmail.com>
2022-09-05 18:58:31 -0300

    first primero

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+const port = 3000;
+app.listen(port, console.log(`Server is Listening on port ${port}...`));
