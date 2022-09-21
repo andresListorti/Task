717ffa215828e2cb4d5ac94b49cd41baa9694ef4
andresListorti <andreslistorti@gmail.com>
2022-09-21 20:11:30 -0300

    mongo-1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/hello", (req, res) => {
 
 app.use("/api/v1/tasks", tasks);
 
-// app.get('/api/v1/task')            - get all the tasks
+// app.get('/api/v1/tasks')            - get all the tasks
 // app.post('/api/v1/tasks')          - create a new task
 // app.get('/api/v1/tasks/:id')       - get single task
 // app.patch('/api/v1/tasks/:id')     - update task
---
+++ db/connect.js
@@ -0,0 +1,2 @@
+const connectionString =
+  "mongodb+srv://pochi:<password>@nodeexpressprojects.geyb9cg.mongodb.net/?retryWrites=true&w=majority";
