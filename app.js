0cc11420e7748bca7f10d6d709dd94c271569994
andresListorti <andreslistorti@gmail.com>
2022-09-22 16:38:48 -0300

    mongoose-1

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("./db/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
@@ -17,6 +18,34 @@ app.use("/api/v1/tasks", tasks);
 // app.get('/api/v1/tasks/:id')       - get single task
 // app.patch('/api/v1/tasks/:id')     - update task
 // app.delete('/api/v1/tasks/:id')    - detele task
+
+// let arr = [1, 2, 3];
+// arr[10] = "hello";
+// console.log(arr.length);
+// console.log(arr);
+
+let clock = () => {
+  let date = new Date();
+  let hrs = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+  let period = "AM";
+  if (hrs == 0) {
+    hrs = 12;
+  } else if (hrs >= 12) {
+    hrs = hrs - 12;
+    period = "PM";
+  }
+  hrs = hrs < 10 ? "0" + hrs : hrs;
+  mins = mins < 10 ? "0" + mins : mins;
+  secs = secs < 10 ? "0" + secs : secs;
+
+  let time = `${hrs}:${mins}:${secs} - ${period}`;
+  console.log(time);
+};
+
+clock();
+// setInterval(clock, 1000);
 
 const port = 3000;
 app.listen(port, console.log(`Server is Listening on port ${port}...`));
--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,14 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://pochi:<password>@nodeexpressprojects.geyb9cg.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://pochi:Pochi_99@nodeexpressprojects.geyb9cg.mongodb.net/Task-Manager?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("CONNECTED WITH THE DB..."))
+  .catch((err) => console.log(err));
